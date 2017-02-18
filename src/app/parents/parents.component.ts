d2f958400b17c962b9d293bf95543c41e9f85156
Mark Goho <markgoho@gmail.com>
2017-02-18 10:48:59 -0500

    Refactor parent component

--- src/app/parents/parents.component.ts
+++ src/app/parents/parents.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { FirebaseService } from '../firebase.service';
 import { AngularFire } from 'angularfire2';
+import { AuthService } from '../auth/auth.service';
 
 import { Parent } from './parent';
 
@@ -16,15 +17,16 @@ export class ParentsComponent implements OnInit {
   activeKey: string;
   appState: string;
   uid: string;
+
   parent: FormGroup;
 
-  constructor (private firebaseService: FirebaseService,
+  constructor (private auth: AuthService,
                 public af: AngularFire,
                 private router: Router,
                 private fb: FormBuilder ) { }
 
   ngOnInit() {
-    this.firebaseService.getParents()
+    this.af.database.list('/parents')
       .subscribe(parents => this.parents = parents);
 
     this.af.auth.subscribe(authData => this.uid = authData.uid);
@@ -37,28 +39,21 @@ export class ParentsComponent implements OnInit {
     });
   }
 
-  addParent(parent) {
-    const createdOn = new Date().toLocaleDateString();
-    parent.fullName = `${parent.firstName} ${parent.lastName}`;
-    parent.createdOn = createdOn;
-    parent.uid = this.uid;
-    console.log(parent);
-    this.firebaseService.addParent(parent);
+  onSubmit() {
+    const formValues = this.parent.value;
+    formValues['fullName'] = `${formValues.firstName} ${formValues.lastName}`;
+    formValues['createdOn'] = new Date().toString();
+    this.af.database.object(`/parents/${this.auth.id}`).update(formValues);
     this.changeState('default');
   }
 
-  changeState(state:string, key:any = null) {
+  changeState(state:string, key:string = null) {
     console.log(`Changing state to ${state}`);
     if (key) {
       console.log(`Changing key to ${key}`);
       this.activeKey = key;
     }
     this.appState = state;
-  }
-
-  orderByProp(parentProp: string) {
-    this.firebaseService.orderByProp(parentProp)
-      .subscribe(parents => this.parents = parents);
   }
 
   goToParent(key) {
